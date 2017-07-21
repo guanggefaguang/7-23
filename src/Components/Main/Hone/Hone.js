38c46170f22ac54faea1d4c5cd6150adc117f687
guanggefaguang <3214558269@qq.com>
2017-07-22 00:57:06 +0800

    7-22

--- src/Components/Main/Hone/Hone.js
+++ src/Components/Main/Hone/Hone.js
@@ -1,23 +1,42 @@
-import React, { Component } from 'react';
+import React, {
+	Component
+} from 'react';
 import './Hone.css';
 
-class Xuzhi extends Component {
-  render() {
-    return (
-     	<div className="Hone">
-     		<div className="banner">
-     			<img src="http://11822707.s61i.faiusr.com/2/AD0I88zRBRACGAAgoNCAwQUo46XggAQwgA84kAM.jpg" />	
-     		</div>
-     		<div className="content">
-     			
-     		</div>
+class Home extends Component {
+	render() {
+		return(
+			<div className="lhgcenter">
+        	<div className="lhgcenterm">
+        			<div className="lhgctitler">
+        				<b>欢迎来到EDUCATION幼儿园</b>
+        				<p>WELCOME TO EDUCATION KINDERGARTEN</p>
+        			</div>
+        	  			<div className="lhglogo">
+        	  	 		<img      src="http://11822707.s61i.faiusr.com/4/AD0I88zRBRAEGAAg4cP-wAUo6drX8QEwqgE4YA.png"/>
+        	 		 		</div>
+        	 		 			<div className="lhglogosr">
+        	 		 			 <div className="lhglogo">
+        	 		 			 	<div className="lhglogleft">
+        	 		 				 <img 														src="http://11822707.s61i.faiusr.c			om/4/AD0I88zRBRAEGAAgtsD-wAUolvfmhgYwSTg-.png"/>
+        	 		 				 </div>
+        	 		 			 		<div  className="lhglogright">
+        	 		 					 <p>0-2岁</p>
+        	 		 			 			<b>148</b>
+        	 		 					 </div>
+        	 		 			 </div>
+        	 		 			 <div className="lgcenter">
+        	 		 			 <p>东方幼儿园创办于1992年9月，一园四址规模， 42个班级，一千多名幼儿,教职工220名。东方幼儿园在育人环境上充分体现尊重与关爱儿童的理念，每处园所构思新颖、独具一格，均设有大型室内溜冰场、室内温水游泳池、大型运动器具、翻斗乐、玩沙池、植物园、浴室、洗衣房等，有设备先进的科技活动室、舞蹈房、建构室、围棋室、图书室、电脑室、钢琴房、古筝室、手风琴房等。各班均有独立的活动室、卧室、饭厅、洗手间、衣帽间等，并配有多媒体电脑、液晶电视、钢琴、录像机、投影仪、冷暖空调、衣橱、小床、玩具橱与桌椅等。</p>
+        	 		 			 </div>
+        	 		 			 <div className="lhgbtn">
+        	 		 			   <a>查看更多</a>
+        	 		 			 </div>
+        	 		 			</div>
+
+        	</div>
      	</div>
- 		    	
-     	
-     
- 
-    );
-  }
+		);
+	}
 }
 
-export default Xuzhi;
+export default Home;
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Banner from './Banner/Banner.js';
-
+import Home from './Hone/Hone';
 
 class Main extends Component{
     render(){
         return (
+        	<div>
             <Banner />
+            <Home />
+            </div>
         )
     }
 }
